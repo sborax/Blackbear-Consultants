5663b6755478dfb7fb0258e773c4ba861aecd58b
James West <james.west2@maine.edu>
2020-04-03 07:40:12 -0400

    initial linking UI to code

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/MainActivity.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.teachingtasks;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText newUsername, newPassword;
+    CheckBox showPass;
+    Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assign variables to UI components
+        newUsername = findViewById(R.id.newUserNameEditText);
+        newPassword = findViewById(R.id.newPasswordEditText);
+        showPass = findViewById(R.id.showPasswordCheckBox);
+        createButton = findViewById(R.id.createUserButton);
+
+
+        //Set EventHandlers
+        createButton.setOnClickListener(this);
+        showPass.setOnClickListener(this);
+
+
+    }
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.showPasswordCheckBox: new ShowPassEventHandler().onCheck(newPassword);
+        }
+
+        if(newUsername.getText().toString().length() > 0 && newPassword.getText().toString().length() > 0){
+            Toast.makeText(getApplicationContext(),newUsername.getText().toString()+"   "+newPassword.getText().toString(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showPassClick(View v) {
+        System.out.println("SHOW PASS CHECK WORKED");
     }
 }
---
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/ShowPassEventHandler.java
@@ -0,0 +1,10 @@
+package com.example.teachingtasks;
+
+import android.widget.EditText;
+
+public class ShowPassEventHandler {
+
+    public void onCheck(EditText pass){
+        pass.setInputType(R.);
+    }
+}
