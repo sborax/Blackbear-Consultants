79eabcfc13195ead9012e2c995ccf1dfea2cf4fa
James West <james.west2@maine.edu>
2020-04-04 06:41:51 -0400

    dynamically place userSelectOptions
    + userSelectOptions now appear in list

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/UserSelectActivity.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/UserSelectActivity.java
@@ -1,17 +1,13 @@
 package com.example.teachingtasks;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,6 +16,7 @@ public class UserSelectActivity extends AppCompatActivity {
     //userOptions limits each device to 20 users
     Button editButton, searchButton;
     TextView userOptions[] = new TextView[20];
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,28 +36,42 @@ public class UserSelectActivity extends AppCompatActivity {
             Intent createUserIntent = new Intent(UserSelectActivity.this, MainActivity.class);
             startActivity(createUserIntent);
         }
-        final TextView temp = new TextView(this);
-
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println(userOptions[0]);
 
-                ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.userSelectLayout);
-                temp.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                temp.setText("tempUser");
-                temp.setTextSize(TypedValue.COMPLEX_UNIT_SP, 48);
-                temp.setTextColor(0xFFFFFFFF);
-                temp.setPadding(0,24,0,0);
-                constraintLayout.addView(temp);
-                userOptions[1] = temp;
+                if(count < 20) {
+                    addUserOption();
+                }
+                else{
+                    //Error message for max users reached
+                }
             }
         });
+    }
+
+    private void addUserOption(){
+        TextView temp = new TextView(this);
+
+        LinearLayout constraintLayout = (LinearLayout) findViewById(R.id.userSelectionLayout);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.CENTER;
+        temp.setLayoutParams(params);
+        temp.setText("tempUser");
+        temp.setTextSize(TypedValue.COMPLEX_UNIT_SP, 48);
+        temp.setTextColor(0xFFFFFFFF);
+        //temp.setPadding(0,24,0,0);
+
+        constraintLayout.addView(temp);
+        userOptions[count] = temp;
+        count++;
+
     }
 
     private void initializeUserOptions() {
         //Grab users from Database
         //Initialize userOptions with users from Database
-        userOptions[0] = (TextView) findViewById(R.id.userSelectOption);
+        userOptions[count] = (TextView) findViewById(R.id.userSelectOption);
+        count++;
     }
 }
