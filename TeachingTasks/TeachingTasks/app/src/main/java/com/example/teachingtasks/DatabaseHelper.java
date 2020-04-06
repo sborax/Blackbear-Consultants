a3a5841a8e095110634730e5fdd42349e6962005
James West <james.west2@maine.edu>
2020-04-06 15:07:01 -0400

    initial database setup
    + Setup database
    + add / remove user methods

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/DatabaseHelper.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.teachingtasks;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,8 +11,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "database";
     public static final String TABLE_NAME = "users";
-    public static final String COL_1 = "username";
-    public static final String COL_2 = "password";
+    public static final String TABLE_ID = "1";
+    public static final String COL_USERNAME = "username";
+    public static final String COL_PASSWORD = "password";
     public static int version = 1;
 
     public DatabaseHelper(Context context) {
@@ -20,11 +22,40 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (" + COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT," + COL_1);
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (" + TABLE_ID + " INTEGER PRIMARY KEY," + COL_USERNAME + " TEXT, " + COL_PASSWORD + " TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public void addUser (String user, String pass){
+        //Add a new user to the database
+
+        //Call Writable
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        //Set the new values
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_USERNAME, user);
+        contentValues.put(COL_PASSWORD, pass);
+
+        //Insert and return
+        db.insert(TABLE_NAME, null, contentValues);
+        return;
+    }
+
+    public void deleteUser(String user){
+        //Delete a user from the database
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String selection = COL_USERNAME + " LIKE ? ";
+
+        String[] selectionArgs = { user };
+
+        db.delete(TABLE_NAME, selection, selectionArgs);
     }
 }
