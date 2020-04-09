f48dc5b0bf050ceffa4821e896cdda75185ce281
James West <james.west2@maine.edu>
2020-04-09 08:38:07 -0400

    fixed colors and layout
    + Updated colors to be working
    + Updated layout for scaling

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/StatisticsActivity.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/StatisticsActivity.java
@@ -1,45 +1,52 @@
 package com.example.teachingtasks;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 
-import android.content.Context;
-import android.os.Bundle;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    PieChart chart;
+    //PieEntry[] pieEntries;
+    PieDataSet pieDataSet;
+    PieData pieData;
+    Legend legend;
+    Description description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
 
-        PieChart chart = (PieChart) findViewById(R.id.chart);
+        chart = (PieChart) findViewById(R.id.chart);
+
+        //This will need to match the styling of whether we use Arrays, Lists, ArrayLists, etc.
         List<PieEntry> pieEntries = Arrays.asList(
                 new PieEntry(6, "Correct"),
                 new PieEntry(3, "Incorrect")
         );
 
-        PieDataSet pieDataSet = new PieDataSet(pieEntries, "SquareData");
-        pieDataSet.setColors(R.color.colorPieGreen, R.color.colorPieRed);
-        PieData pieData = new PieData(pieDataSet);
+        pieDataSet = new PieDataSet(pieEntries, "SquareData");
+        pieDataSet.setColors(new int[] { R.color.colorPieGreen, R.color.colorPieRed }, StatisticsActivity.this);
+
+        pieData = new PieData(pieDataSet);
 
         chart.setData(pieData);
         chart.setCenterText("Square Identification");
-        chart.setHoleRadius(40f);
-        Legend legend = chart.getLegend();
+        chart.setHoleRadius(50f);
+        legend = chart.getLegend();
         legend.setEnabled(false);
-        Description description = chart.getDescription();
+        description = chart.getDescription();
         description.setEnabled(false);
     }
 }
