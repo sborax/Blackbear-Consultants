ae9006f43db5f0fa1d6a5288aec7027aa183797d
James West <james.west2@maine.edu>
2020-04-13 09:21:49 -0400

    Removed Header & fixed background img
    + Removed blue Header
    + Fixed chalkboard background to rescale properly on devices

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/RegisterUserActivity.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/RegisterUserActivity.java
@@ -3,6 +3,7 @@ package com.example.teachingtasks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import android.view.View;
