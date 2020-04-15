981dd256e750f88265e80dd6c1ab909eec0dc348
James West <james.west2@maine.edu>
2020-04-15 09:17:44 -0400

    temp Intent for testing
    + Add temp intent when clicking Task button to go to Login screen

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/GameActivity.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/GameActivity.java
@@ -2,8 +2,12 @@ package com.example.teachingtasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
@@ -17,5 +21,22 @@ public class GameActivity extends AppCompatActivity {
 
         username = (TextView) findViewById(R.id.username);
         username.setText(getIntent().getStringExtra("EXTRA_USER"));
+
+        ImageButton imageView = (ImageButton) findViewById(R.id.task);
+
+        /*
+        TEMP EVENT HANDLER
+         */
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                Intent loginIntent = new Intent(GameActivity.this, LoginActivity.class);
+                loginIntent.putExtra("EXTRA_USER", username.getText());
+                GameActivity.this.startActivity(loginIntent);
+                return false;
+            }
+        });
     }
 }
