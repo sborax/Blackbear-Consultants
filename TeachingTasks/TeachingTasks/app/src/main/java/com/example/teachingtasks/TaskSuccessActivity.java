2cef7b05cedb220dc22d4d651e9f9fd1bddc8e19
James West <james.west2@maine.edu>
2020-04-15 16:35:23 -0400

    TouchEvent setup
    + If you click on the screen, takes you back to the GameActivity

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/TaskSuccessActivity.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/TaskSuccessActivity.java
@@ -2,13 +2,34 @@ package com.example.teachingtasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class TaskSuccessActivity extends AppCompatActivity {
+
+    TextView username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_success);
+
+        username = (TextView) findViewById(R.id.username);
+        username.setText(getIntent().getStringExtra("EXTRA_USER"));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+
+        if (e.getAction() == MotionEvent.ACTION_DOWN){
+
+            Intent gameIntent = new Intent(TaskSuccessActivity.this, GameActivity.class);
+            gameIntent.putExtra("EXTRA_USER", username.getText());
+            TaskSuccessActivity.this.startActivity(gameIntent);
+        }
+        return false;
     }
 }
