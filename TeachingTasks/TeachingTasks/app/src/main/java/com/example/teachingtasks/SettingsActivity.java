fd1d121915a3c4cff4dccbf31bd327460425d68e
James West <james.west2@maine.edu>
2020-04-26 14:28:25 -0400

    temp logout for testing

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/SettingsActivity.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/SettingsActivity.java
@@ -2,7 +2,9 @@ package com.example.teachingtasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -12,5 +14,16 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+
+        if (e.getAction() == MotionEvent.ACTION_DOWN){
+
+            Intent userSelectIntent = new Intent(SettingsActivity.this, UserSelectActivity.class);
+            SettingsActivity.this.startActivity(userSelectIntent);
+        }
+        return false;
     }
 }
