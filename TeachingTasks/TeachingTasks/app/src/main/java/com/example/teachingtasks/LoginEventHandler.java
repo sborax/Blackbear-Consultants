18fd1bf96afa3e94751f5670e87b2d2b5e19f01b
abdul_karim <abdullah.karim@maine.edu>
2020-04-27 06:00:23 -0400

    Mapped login button to game selection

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/LoginEventHandler.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/LoginEventHandler.java
@@ -11,13 +11,20 @@ public class LoginEventHandler {
 
         if(mydb.userPasswordCheck(username, passwordAttempt)){
 
-            //Change this to GameSelectionActivity once it's ready
-            System.out.println("LoginEventHandler.java requires access to GameSelectionActivity...");
-
+            //Change this to GameSelectionActivity once it's ready.
+            //This will get moved to the start button.
+//            System.out.println("LoginEventHandler.java requires access to GameSelectionActivity...");
+//
+//            mydb.close();
+//            Intent gameIntent = new Intent(loginActivity.getBaseContext(), GameActivity.class);
+//            gameIntent.putExtra("EXTRA_USER", username);
+//            loginActivity.startActivity(gameIntent);
+            
+            //The user entered the right login information, bring them to game selection menu.
+            System.out.println("Authentication successful. Going to Game Selection.");
             mydb.close();
-            Intent gameIntent = new Intent(loginActivity.getBaseContext(), GameActivity.class);
-            gameIntent.putExtra("EXTRA_USER", username);
-            loginActivity.startActivity(gameIntent);
+            Intent gameSelectionIntent = new Intent(loginActivity.getBaseContext(), GameSelectionActivity.class);
+            loginActivity.startActivity(gameSelectionIntent);
         }
         else{
             Toast.makeText(loginActivity, "Incorrect Password", Toast.LENGTH_SHORT).show();
