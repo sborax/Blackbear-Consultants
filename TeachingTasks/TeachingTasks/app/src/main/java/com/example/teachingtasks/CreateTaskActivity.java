9c96547c16df5eb93d88c09a3df09bc78798c38b
EMLLevy <skyzoe45@gmail.com>
2020-05-01 10:31:28 -0400

    Create Task activity has been fixed, and all buttons work as currently intended

--- TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/CreateTaskActivity.java
+++ TeachingTasks/TeachingTasks/app/src/main/java/com/example/teachingtasks/CreateTaskActivity.java
@@ -1,11 +1,18 @@
 package com.example.teachingtasks;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileNotFoundException;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CreateTaskActivity extends AppCompatActivity {
@@ -14,7 +21,10 @@ public class CreateTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
 
+        
         taskNavButton = (Button) findViewById(R.id.taskNavButton);
         statisticsNavButton = (Button) findViewById(R.id.statisticsNavButton);
         settingsNavButton = (Button) findViewById(R.id.settingsNavButton);
@@ -22,8 +32,7 @@ public class CreateTaskActivity extends AppCompatActivity {
         username = (TextView) findViewById(R.id.username);
         username.setText(getIntent().getStringExtra("EXTRA_USER"));
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_task);
+
 
         taskNavButton.setOnClickListener(new View.OnClickListener() {
             @Override
